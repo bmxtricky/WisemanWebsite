82fcba007919c09008c1523f96d34df101612cec
bmxtricky5 <mwoodbert@gmail.com>
2021-08-26 22:38:43 -0700

    updated color further speced out javascript

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,6 +1,12 @@
+import Home from './views/Home.js';
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+}
+
 const router = async () => {
     const routes = [
-        {path: '/', view: () => console.log('Home')},
+        {path: '/', view: Home},
         {path: '/shop', view: () => console.log('shop')},
         {path: '/about', view: () => console.log('about')},
         {path: '/contact', view: () => console.log('contact')},
@@ -12,5 +18,28 @@ const router = async () => {
             isMatch: location.pathname === route.path
         };
     });
-    console.log(potentialMatches);
+
+    let match = potentialMatches.find(potentialMatches => potentialMatches.isMatch);
+    if (!match) {
+        match = {
+            route: routes[0], 
+            isMatch: true
+        };
+    };
+
+    const view = new match.route.view();
+
+    document.querySelector('#content').innerHTML = await view.getHtml();
+
+    console.log(new match.route.view());
 };
+window.addEventListener('popstate', router);
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.addEventListener('click', e => {
+        if (e.target.matches('[data-link]')) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    })
+    router();
+});
