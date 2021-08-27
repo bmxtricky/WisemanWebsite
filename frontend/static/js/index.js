a4e2a3e24f8ca5fbd414573431dfc5016130b9c1
bmxtricky5 <mwoodbert@gmail.com>
2021-08-26 21:17:00 -0700

    updates

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,1 +1,16 @@
-console.log('JS is loaded');
+const router = async () => {
+    const routes = [
+        {path: '/', view: () => console.log('Home')},
+        {path: '/shop', view: () => console.log('shop')},
+        {path: '/about', view: () => console.log('about')},
+        {path: '/contact', view: () => console.log('contact')},
+    ];
+
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        };
+    });
+    console.log(potentialMatches);
+};
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
-app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', ''));
-});
 app.use('/style.css', express.static(path.resolve(__dirname, 'frontend', 'style.css')));
+app.use('/images', express.static(path.resolve(__dirname, 'frontend', 'images')));
+app.use('/static', express.static(path.resolve(__dirname, 'frontend', 'static')));
+app.get('/*', (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, 'frontend', 'index.html'));
+});
 app.listen(process.env.PORT || 50600, () => console.log('Server running...'));
