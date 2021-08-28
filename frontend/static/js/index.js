ae25cf7ac2067fe5d380fae3d6061f55ee780829
bmxtricky5 <mwoodbert@gmail.com>
2021-08-28 16:11:06 -0700

    fixng more stuff

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -6,7 +6,7 @@ import Contact from './views/Contact.js';
 const pathToRegex = path => new RegExp('^' + path.replace(/\//g, '\\/').replace(/:\w+/g, '(.+)') + "$");
 
 const getParams =   match => {
-    const values = match.result.slice(1);
+    const values = match.result.slice(1, 2);
     const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
     console.log(keys)
     return Object.fromEntries(keys.map((key, i) => {
